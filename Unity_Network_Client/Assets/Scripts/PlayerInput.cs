6f13bf17af905de1f66e328ae9368709adf901ce
Haesst <robin.ortryd@futuregames.nu>
2019-08-16 16:36:23 +0200

    Movement Added a space background Added spaceship and ammunition sprites Started with player movement

---
+++ Unity_Network_Client/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] float rotationSpeed = 400;
+    [SerializeField] float speed = 1000;
+
+    private void Awake()
+    {
+        // Set the rigidbody
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        float thrust = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0,0,-1) * rotation);
+        rb.AddForce(transform.up * thrust);
+    }
+}
